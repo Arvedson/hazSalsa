932ce208863ed90a7814e15d3c3a28ecb4ab42b6
Arvedson <tomasarvedson@gmail.com>
2024-06-03 09:34:17 -0600

    deployFix.3

--- src/app/api/users/users.tsx
+++ src/app/api/users/users.tsx
@@ -1,9 +1,10 @@
-// pages/api/users.js
+// src/app/api/users/users.tsx
 
+import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../../lib/db/mongodb';
 import User from "../../../lib/db/models/User"
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
 
   await dbConnect();
@@ -21,7 +22,7 @@ export default async function handler(req, res) {
       try {
         const user = await User.create(req.body);
         res.status(201).json({ success: true, data: user });
-      } catch (error) {
+      } catch (error: any) {
         res.status(400).json({ success: false, error: error.message });
       }
       break;
